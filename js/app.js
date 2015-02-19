1046779d63a9eba57290d71f0d56ab92c9cf55e8
Matt Hampel <matth@localdata.com>
2015-02-19 16:39:33 -0500

    Start at a beter center

--- js/app.js
+++ js/app.js
@@ -15,7 +15,7 @@ $(function(){
   L.mapbox.accessToken = 'pk.eyJ1IjoibWF0dGgiLCJhIjoicGFzV1ZkWSJ9.KeK3hKmM52XpUEHHx_F8NQ';
   var map = L.mapbox.map('map', 'matth.l8e9ppm3', {
     maxZoom: 16
-  }).setView([41.6278, -83.6265], 13);
+  }).setView([41.674609,-83.574028], 13);
 
   map.addControl(L.mapbox.geocoderControl('mapbox.places'));
 
